99b54126f492b87c48361503fafefa4b0885e5d9
marca116 <marca116@hotmail.com>
2025-07-01 12:31:23 -0400

    Add full install

---
+++ setup/ollama_build/full_install_and_launch_pipeline.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source ./setup/ollama_build/install_venv.sh &&
+source ./run_app_ollama.sh
---
+++ setup/vllm_build/full_install_and_launch_pipeline.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source ./setup/vllm_build/install_venv_vllm.sh &&
+source ./run_app_vllm.sh
